e19cbe5cf70e16cf9126873188dcec4ab65cd1f5
Postolovski Dragan <d.postolovski@gmail.com>
2019-01-17 11:37:31 +0100

    Update temp_functions.py
    Edited the function so it returns :
    - number of words
    - number of links
    - number of code blocks
    
    Added import line at the top

--- temp_functions.py
+++ temp_functions.py
@@ -1,8 +1,11 @@
-# use this function to filter noise from each post's body
+# import re
+
 def filter_body_noise(input):
 
     # count blocks of code
-    code_blocks = len(re.findall(r"</code>", input))
+    nr_code_blocks = len(re.findall(r"</code>", input))
+    # count link in body
+    nr_links = len(re.findall(r"</a>", input))
 
     # couldnt make the regex work with multi-line files, so I remove them :))))
     input = input.replace('\n', ' ')
@@ -17,5 +20,13 @@ def filter_body_noise(input):
     # remove hardcoded links
     input = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', input, flags=re.MULTILINE)
 
-    # returns tuple in format (filtered_body, number_of_code_blocks)
-    return (input,code_blocks)
+    # remove multiple whitespace characters to clean up the previous
+    # string substitutions(because when removing for example a code block, there will be two spaces
+    # remaining on each side of the code block, thus we get two whitespace characters
+    input = re.sub("\s\s+", ' ', input)
+
+    # count remaining words
+    nr_words = len(input.split(' '))
+
+    # returns tuple in format (number_of_words, number_of_links, number_of_code_blocks)
+    return (nr_words, nr_links, nr_code_blocks)
