51a76665e7fe70c3f58aa26c526d5d0b01ded656
wojtas1411 <wojtas.t.1997@gmail.com>
2019-01-17 22:15:52 +0100

    playground v9.9 some cells refactoring

--- test_all_dataframes.py
+++ test_all_dataframes.py
@@ -1,10 +1,18 @@
 import dask.dataframe as dd
+from multiprocessing import Pool
+
+## lets go parallel
+
+def process_df(filename):
+    temp_df = dd.read_csv(filename+"-0.csv.tar.xz",compression="xz",blocksize=None)
+    print(filename + "\n", temp_df.head(),"\n", temp_df.dtypes, "\n-----\n")
+
+p = Pool()
 
 filenames = ["BountyFrame", "DuplicateFrame", "TagsFrame", "UsersFrame", "BadgesFrame", "BadgesAggregatedFrame"]
 
-for filename in filenames:
-    print(filename)
-    temp_df = dd.read_csv(filename+"-0.csv.tar.xz",compression="xz",blocksize=None)
-    print(temp_df.head())
-    print(temp_df.dtypes)
-    print("-----")
+p.map(process_df, filenames)
+
+#for filename in filenames:
+#    process_df(filename)
+    
--- test_tags_dataframe.py
+++
@@ -1,5 +0,0 @@
-import dask.dataframe as dd
-
-df = dd.read_csv("TagsFrame-0.csv.tar.xz",compression="xz",blocksize=None)
-print(df.head())
-print(df.dtypes)
